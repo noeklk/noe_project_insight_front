e51bcb4df6096b972c7420aa88f5550ffaba45a6
Noé <noe.abdelkalek@gmail.com>
2020-02-06 23:11:06 +0100

    Initialisation des fondations

--- src/nimn-front-app/src/app/app.module.ts
+++ src/nimn-front-app/src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material-module";
+import { UserComponent } from "./component/user/user.component";
+import { ModuleComponent } from "./component/module/module.component";
+import { SessionComponent } from "./component/session/session.component";
+import { NoteComponent } from "./component/note/note.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ModuleComponent,
+    SessionComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
